6ed58ba97c2e901bf7d5b38cafc38fdf1e80fe5f
xiangwen94 <xiangwen94@gmail.com>
2018-01-03 17:44:30 +0800

    paypal v4.4: save shipping address into session

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -59,7 +59,6 @@ class CheckoutController extends Controller
 
     public function checkout()
     {
-      dd(session()->all());
       if(sizeof(session('cart.item')) == 0 || session('cart.shipping.location') == '')
           return redirect()->back();
       else
