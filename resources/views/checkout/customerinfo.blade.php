14eb2b129addb33e406f34ef58540c0e175eb372
xiangwen94 <xiangwen94@gmail.com>
2018-01-03 21:43:18 +0800

    paypal v5.17

--- resources/views/checkout/customerinfo.blade.php
+++ resources/views/checkout/customerinfo.blade.php
@@ -62,7 +62,7 @@
             </div>
             <div class="col-md-6 col-md-pull-4 col-xs-10 col-xs-offset-1">
               <div class="subtitle">Customer Details</div>
-              <form name="customerDetail">
+              <form action="/shipping/save" method="post" name="customerDetail">
                 {{ csrf_field() }}
                 <div class="form-group">
                   <label for="email">Email
