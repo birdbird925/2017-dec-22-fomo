96e6d03127179c3a85f7013b05a838d253a400bb
xiangwen94 <xiangwen94@gmail.com>
2018-01-03 17:04:30 +0800

    paypal v3.25: create payment by server side REST api call

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -178,22 +178,22 @@ class CheckoutController extends Controller
           $items->addItem($item);
       }
 
-      $item = PayPal::Item();
-      $item->setName('Shipping Fee');
-      $item->setQuantity(1);
-      $item->setCurrency(session('currency'));
-      $item->setPrice(session('cart.shipping.cost'));
-      $items->addItem($item);
+      // $item = PayPal::Item();
+      // $item->setName('Shipping Fee');
+      // $item->setQuantity(1);
+      // $item->setCurrency(session('currency'));
+      // $item->setPrice(session('cart.shipping.cost'));
+      // $items->addItem($item);
 
       // discount
-      if(session()->has('checkout.voucher.value')) {
-        $item = PayPal::Item();
-        $item->setName('Discount');
-        $item->setQuantity(1);
-        $item->setCurrency(session('currency'));
-        $item->setPrice(-session('checkout.voucher.value'));
-        $items->addItem($item);
-      }
+      // if(session()->has('checkout.voucher.value')) {
+      //   $item = PayPal::Item();
+      //   $item->setName('Discount');
+      //   $item->setQuantity(1);
+      //   $item->setCurrency(session('currency'));
+      //   $item->setPrice(-session('checkout.voucher.value'));
+      //   $items->addItem($item);
+      // }
 
       $transaction = PayPal::Transaction();
     	$transaction->setAmount($amount);
