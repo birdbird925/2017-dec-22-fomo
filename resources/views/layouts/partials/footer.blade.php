481f6d5f2e4e356754eb1d70165c3e49079bc97b
xiangwen94 <xiangwen94@gmail.com>
2018-01-15 14:32:44 +0800

    footer remove design by area

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -14,6 +14,6 @@
         &copy; {{ date("Y") == '2017' ? '2017' : "2017 - ".date('Y') }} FOMO
     </div>
     <div class="design-by">
-        Website by: <a href="http://www.peiyingtang.com" target="_blank">PY</a> + <a href="http://www.thelittletroop.com" target="_blank">LOOI</a>
+        {{-- Website by: <a href="http://www.peiyingtang.com" target="_blank">PY</a> + <a href="http://www.thelittletroop.com" target="_blank">LOOI</a> --}}
     </div>
 </footer>
