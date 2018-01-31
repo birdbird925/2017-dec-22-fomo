90cd2271076e2ddc9e93f45a9eba50d6a7d4f6fd
xiangwen94 <xiangwen94@gmail.com>
2018-01-31 20:00:28 +0800

    disable product status checking

--- app/CustomizeProduct.php
+++ app/CustomizeProduct.php
@@ -25,67 +25,68 @@ class CustomizeProduct extends Model
 
     public function checkComponentStatus()
     {
-        $images = json_decode($this->images);
-        $radioInput = json_decode($this->components);
-        $sizeComponentID = 0;
-        $personalizeImage = false;
-        $components = [];
-
-
-        foreach($radioInput as $name=>$input) {
-            if($name == 'customize_type') {
-                array_push($components, $this->type);
-            }
-            // if input was extral radio button
-            else if(substr($name, -6) == 'extral') {
-                $component = CustomizeComponentOption::find($input->value);
-                if($component) array_push($components, $component);
-            }
-            // input is personalize item
-            else if (strpos($name, 'personalize') !== false){
-                if(isset($input->rotation))
-                    $personalizeImage = true;
-            }
-            // if input was main radio button
-            else {
-                $component = CustomizeComponent::find($input->value);
-                if($component) {
-                    // check is size component
-                    if($component->size_component) $sizeComponentID = $input->value;
-                    array_push($components, $component);
-                }
-            }
-        }
-
-        foreach($components as $component) {
-            if($sizeComponentID != 0 && $component->size_image) {
-                $sizeImage = json_decode($component->size_image);
-
-                if(isset($sizeImage->$sizeComponentID->front_image)) {
-                    $frontImage = $sizeImage->$sizeComponentID->front_image;
-                    if (($key = array_search(Image::find($frontImage)->getSrc(), $images)) !== false)
-                        unset($images[$key]);
-                }
-
-                if(isset($sizeImage->$sizeComponentID->back_image)) {
-                    $backImage = $sizeImage->$sizeComponentID->back_image;
-                    if (($key = array_search(Image::find($backImage)->getSrc(), $images)) !== false)
-                        unset($images[$key]);
-                }
-
-            }
-            else {
-                if($component->front_image)
-                    if (($key = array_search($component->image('front_image')->getSrc(), $images)) !== false)
-                        unset($images[$key]);
-
-                if($component->back_image)
-                    if (($key = array_search($component->image('back_image')->getSrc(), $images)) !== false)
-                        unset($images[$key]);
-            }
-
-        }
-
-        return ($personalizeImage ? sizeof($images) == 1 : sizeof($images) == 0);
+        // $images = json_decode($this->images);
+        // $radioInput = json_decode($this->components);
+        // $sizeComponentID = 0;
+        // $personalizeImage = false;
+        // $components = [];
+        //
+        //
+        // foreach($radioInput as $name=>$input) {
+        //     if($name == 'customize_type') {
+        //         array_push($components, $this->type);
+        //     }
+        //     // if input was extral radio button
+        //     else if(substr($name, -6) == 'extral') {
+        //         $component = CustomizeComponentOption::find($input->value);
+        //         if($component) array_push($components, $component);
+        //     }
+        //     // input is personalize item
+        //     else if (strpos($name, 'personalize') !== false){
+        //         if(isset($input->rotation))
+        //             $personalizeImage = true;
+        //     }
+        //     // if input was main radio button
+        //     else {
+        //         $component = CustomizeComponent::find($input->value);
+        //         if($component) {
+        //             // check is size component
+        //             if($component->size_component) $sizeComponentID = $input->value;
+        //             array_push($components, $component);
+        //         }
+        //     }
+        // }
+        //
+        // foreach($components as $component) {
+        //     if($sizeComponentID != 0 && $component->size_image) {
+        //         $sizeImage = json_decode($component->size_image);
+        //
+        //         if(isset($sizeImage->$sizeComponentID->front_image)) {
+        //             $frontImage = $sizeImage->$sizeComponentID->front_image;
+        //             if (($key = array_search(Image::find($frontImage)->getSrc(), $images)) !== false)
+        //                 unset($images[$key]);
+        //         }
+        //
+        //         if(isset($sizeImage->$sizeComponentID->back_image)) {
+        //             $backImage = $sizeImage->$sizeComponentID->back_image;
+        //             if (($key = array_search(Image::find($backImage)->getSrc(), $images)) !== false)
+        //                 unset($images[$key]);
+        //         }
+        //
+        //     }
+        //     else {
+        //         if($component->front_image)
+        //             if (($key = array_search($component->image('front_image')->getSrc(), $images)) !== false)
+        //                 unset($images[$key]);
+        //
+        //         if($component->back_image)
+        //             if (($key = array_search($component->image('back_image')->getSrc(), $images)) !== false)
+        //                 unset($images[$key]);
+        //     }
+        //
+        // }
+        //
+        // return ($personalizeImage ? sizeof($images) == 1 : sizeof($images) == 0);
+        return true;
     }
 }
