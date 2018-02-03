6062dc38a7202860d04d39cd834e2af4a816e0e5
xiangwen94 <xiangwen94@gmail.com>
2018-02-03 13:18:29 +0800

    personalize text font size for backcase

--- resources/views/customize.blade.php
+++ resources/views/customize.blade.php
@@ -112,8 +112,8 @@
                                 @endforeach
                             @endif
                             <div class="description">
-                                <div class="main"></div>
-                                <div class="extral"></div>
+                                <div class="main">1212</div>
+                                <div class="extral">1212</div>
                             </div>
                         </div>
                     </li>
