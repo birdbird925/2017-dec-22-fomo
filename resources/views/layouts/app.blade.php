2c9282047cae80799c6b08a5895190b9bc0cafea
xiangwen94 <xiangwen94@gmail.com>
2018-02-22 14:21:15 +0800

    update page title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+    <title>FOMO, A full customization experience for your watch</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
