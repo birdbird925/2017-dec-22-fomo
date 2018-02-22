6ce2decc69387e62adbbee8f707a00898cf950c0
xiangwen94 <xiangwen94@gmail.com>
2018-02-22 14:06:23 +0800

    login popup error update

--- public/js/main.js
+++ public/js/main.js
@@ -406,15 +406,17 @@ $(function() {
         var password = form.find('input[type=password]').val();
         var action = $(this).attr('data-action');
 
-        if(password == '') {
-            form.find('input[type=password]').addClass('animated shake errorInput');
-            setTimeout(function() { form.find('input[type=password]').removeClass('animated shake'); }, 1000);
+        if(password == '' || email == '') {
+            if(password == '') {
+                form.find('input[type=password]').addClass('animated shake errorInput');
+                setTimeout(function() { form.find('input[type=password]').removeClass('animated shake'); }, 1000);
+            }
+
+            if(email == '') {
+                form.find('input[type=email]').addClass('animated shake errorInput');
+                setTimeout(function() { form.find('input[type=email]').removeClass('animated shake'); }, 1000);
+            }
             // msgPopup('Erm', 'Don\'t fill up the form is not cool!');
-        }
-        else if(email == '') {
-            form.find('input[type=email]').addClass('animated shake errorInput');
-            setTimeout(function() { form.find('input[type=email]').removeClass('animated shake'); }, 1000);
-            // msgPopup('Erm', 'Are you sure it is email?');
         }
         else if(!emailRE.test(email)){
             form.find('input[type=email]').addClass('animated shake errorInput');
