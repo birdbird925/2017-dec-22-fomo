96e3892a3ec0dd33e37318b868cd7514ccee379b
xiangwen94 <xiangwen94@gmail.com>
2018-03-14 13:33:05 +0800

    facebook pixel update

--- public/js/main.js
+++ public/js/main.js
@@ -477,14 +477,6 @@ $(function() {
                             }
                             if(url == '/register'){
                                 msgPopup('OH Yeah!', 'Thanks for the registration.');
-                                // !function(f,b,e,v,n,t,s)
-                                // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                                // n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                                // if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                                // n.queue=[];t=b.createElement(e);t.async=!0;
-                                // t.src=v;s=b.getElementsByTagName(e)[0];
-                                // s.parentNode.insertBefore(t,s)}(window,document,'script',
-                                // 'https://connect.facebook.net/en_US/fbevents.js');
                                 fbq('init', '377930176006641');
                                 fbq('track', 'CompleteRegistration');
                             }
@@ -1716,16 +1708,6 @@ $(function() {
                 saveBtn.removeClass('save')
                 saveBtn.addClass('saved');
                 saveBtn.attr('data-id', id);
-
-                // facebook pixel add to cart
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window,document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '377930176006641');
                 fbq('track', 'AddToWishlist');
             }
@@ -2039,16 +2021,6 @@ $(function() {
                 // cartBtn.attr('data-id', cartCode);
                 var cartCount = parseInt($('.cart > span').text());
                 $('.cart > span').text(cartCount+1);
-
-                // facebook pixel add to cart
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window,document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '377930176006641');
                 fbq('track', 'AddToCart');
             }
